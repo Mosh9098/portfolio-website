b258b670bb6ebbb8d9ac31fee371125dcfeaf8a4
mosh9098 <mosh9098@gmail.com>
2025-01-08 20:32:54 +0300

    images fixed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const HeroSection = () => (
   <section id="hero" className="hero-section">
-    <img src='/public/me4.jpg' alt="David Maina" className="hero-image" />
+    <img src='/me4.jpg' alt="David Maina" className="hero-image" />
     <div className="hero-content">
       <h1>Hello, I'm David Maina. I'm a Software Developer.</h1>
       {/* <p>I create interactive and responsive web experiences.</p> */}
