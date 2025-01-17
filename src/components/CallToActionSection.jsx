bdd3e1f594c5950a8b940e9994cd327c2f518359
mosh9098 <mosh9098@gmail.com>
2025-01-17 11:31:37 +0300

    updated files fixed

--- src/components/CallToActionSection.jsx
+++ src/components/CallToActionSection.jsx
@@ -4,8 +4,8 @@ export const CallToActionSection = () => (
   <section className="cta-section">
     <h2>For More Information about me</h2>
     <div className="cta-buttons">
-      <a href="src/files/David Maina Resume.pdf" download className="download-btn">Download Resume</a>
-      <a href="src/files/David Maina Education.pdf" download className="download-btn">Download Education</a>
+      <a href="/David Maina Resume.pdf" download className="download-btn">Download Resume</a>
+      <a href="/David Maina Education.pdf" download className="download-btn">Download Education</a>
       <a href="mailto:mosh9098@gmail.com" className="hire-btn">Hire Me</a>
     </div>
   </section>
